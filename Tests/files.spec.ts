5b0abc8b12c4b7be9ba72c4e24ceac60770db54a
MohammadNomani <mohammad.nomani@proactive.dk>
2018-08-14 11:31:12 +0200

    base build setup and readme

---
+++ Tests/files.spec.ts
@@ -0,0 +1,24 @@
+﻿let fs = require("fs");
+let vendorGraphExists: boolean = false;
+let vendorLibraryExists: boolean = false;
+let fileSizeInBytes: number = 0;
+
+describe('Application Insights', () => {
+    beforeAll(async () => {
+        vendorGraphExists = fs.existsSync('./mtno-insights-manifest.json');
+        vendorLibraryExists = fs.existsSync('./mtno.insights.js');
+        fileSizeInBytes = fs.statSync('./mtno.insights.js').size;
+    });
+
+    test("DLL Plugin Graph Exists", async () => {
+        expect(vendorGraphExists).toBeTruthy();
+    });
+
+    test("DLL Plugin Library Exists", async () => {
+        expect(vendorLibraryExists).toBeTruthy();
+    });
+
+    test("Library Has Content", async () => {
+        expect(fileSizeInBytes).toBeGreaterThan(5000);
+    });
+});
